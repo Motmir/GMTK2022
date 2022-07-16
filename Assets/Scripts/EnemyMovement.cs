6fa5bcf39f654fa15905726fd70aab8221ddd1fd
Eirik Akerhaugen <Eirik_akerhaugen@hotmail.com>
2022-07-16 06:28:29 +0200

    Update EnemyMovement.cs

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,6 +7,7 @@ public class EnemyMovement : MonoBehaviour
 
     GameObject player;
     public float detectionRange;
+    bool madeLine;
 
     // Start is called before the first frame update
     void Start()
@@ -29,9 +30,24 @@ public class EnemyMovement : MonoBehaviour
     void FindPlayer()
     {
         Vector3 distance = player.transform.position - transform.position;
+        //Debug.Log("Dist: " + distance.magnitude);
 
-        if (distance.magnitude < detectionRange)
+        if (distance.magnitude < detectionRange && !madeLine)
         {
+            madeLine = true;
+            LineRenderer lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
+            lineRenderer.startColor = Color.black;
+            lineRenderer.endColor = Color.black;
+            lineRenderer.startWidth = 0.01f;
+            lineRenderer.endWidth = 0.01f;
+            lineRenderer.positionCount = 2;
+            lineRenderer.useWorldSpace = true;
+            lineRenderer.sortingOrder = 1;
+            lineRenderer.sortingLayerName = "Entity";
+
+            //For drawing line in the world space, provide the x,y,z values
+            lineRenderer.SetPosition(0, transform.position); //x,y and z position of the starting point of the line
+            lineRenderer.SetPosition(1, player.transform.position);
             Debug.Log("I see you!");
         }
     }
